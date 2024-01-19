778444b2d18c7953226448daa4f90ace85cabb7f
krishna <krishnajain5050@gmail.com>
2024-01-19 19:01:23 +0530

    usd sol inr

---
+++ DepthManager.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+export class DepthManager {
+    private market: string;
+    private bids: {
+        [key: string]: string
+    };
+    private asks: {
+        [key: string]: string
+    };
+    constructor(market: string) {
+        this.market = market;
+        setInterval(() => {
+            this.pollMarket();
+        }, 1000);
+    }
+
+    pollMarket = async () => {
+        const res = await fetch(`https://public.coindcx.com/market_data/orderbook?pair=${this.market}`);
+        const depth = await res.json();
+        this.bids = depth?.bids;
+        this.asks = depth?.asks;
+    }
+
+    getRelevantDepth = () => {
+        let highestBid = -100;
+        let lowestAsk = 10000000;
+        Object.keys(this.bids).map(x => {
+            if (parseFloat(x) > highestBid) {
+                highestBid = parseFloat(this.bids[x]);
+            }
+        });
+        Object.keys(this.asks).map(x => {
+            if (parseFloat(x) < lowestAsk) {
+                lowestAsk = parseFloat(this.asks[x]);
+            }
+        });
+        return {
+            highestBid,
+            lowestAsk
+        }
+    }
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
-console.log("Hello via Bun!");
+import { DepthManager } from "./DepthManager";
+
+const solInrMarket = new DepthManager("B-SOL_INR");
+const usdtInrMarket = new DepthManager("B-USDT_INR");
+const solUsdtMarket = new DepthManager("B-SOL_USDT");
+
+setInterval(() => {
+    console.log(solInrMarket.getRelevantDepth());
+    console.log(usdtInrMarket.getRelevantDepth());
+    console.log(solUsdtMarket.getRelevantDepth());
+
+    // sell SOL for INR, buy USDT from INR, buy SOL from INR
+
+    const canGetInr = solInrMarket.getRelevantDepth().highestBid - 0.001;
+    const canGetUsdt = canGetInr / usdtInrMarket.getRelevantDepth().lowestAsk;
+    const canGetSol = canGetUsdt / solInrMarket.getRelevantDepth().lowestAsk;
+
+    console.log(`You can convert 1 SOL into ${canGetSol} SOL`);
+
+    const initialInr = solInrMarket.getRelevantDepth().lowestAsk + 0.001;
+    const canGetUsdt2 = usdtInrMarket.getRelevantDepth().highestBid;
+    const canGetInr2 = usdtInrMarket.getRelevantDepth().highestBid * canGetUsdt2;
+
+    console.log(`you converted ${initialInr} sol into ${canGetInr2} INR`);
+
+}, 2000);
