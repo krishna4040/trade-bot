46e32b87a25a8986791b6a811140585582a06167
krishna jain <115697713+krishna4040@users.noreply.github.com>
2024-01-19 19:56:38 +0530

    place order and cacel order

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { DepthManager } from "./DepthManager";
+import { cancelAll, createOrder } from "./order";
 
 const solInrMarket = new DepthManager("B-SOL_INR");
 const usdtInrMarket = new DepthManager("B-USDT_INR");
@@ -10,6 +11,7 @@ setInterval(() => {
     console.log(solUsdtMarket.getRelevantDepth());
 
     // sell SOL for INR, buy USDT from INR, buy SOL from INR
+    // lets say u start with 1 sol
 
     const canGetInr = solInrMarket.getRelevantDepth().highestBid - 0.001;
     const canGetUsdt = canGetInr / usdtInrMarket.getRelevantDepth().lowestAsk;
@@ -17,10 +19,23 @@ setInterval(() => {
 
     console.log(`You can convert 1 SOL into ${canGetSol} SOL`);
 
-    const initialInr = solInrMarket.getRelevantDepth().lowestAsk + 0.001;
-    const canGetUsdt2 = usdtInrMarket.getRelevantDepth().highestBid;
-    const canGetInr2 = usdtInrMarket.getRelevantDepth().highestBid * canGetUsdt2;
+    const initialInr = solInrMarket.getRelevantDepth().highestBid + 0.001;
+    const canGetUsdt2 = solUsdtMarket.getRelevantDepth().highestBid;
+    const canGetInr2 = usdtInrMarket.getRelevantDepth().highestBid / canGetUsdt2;
 
-    console.log(`you converted ${initialInr} sol into ${canGetInr2} INR`);
+    console.log(`you converted ${initialInr} INR into ${canGetInr2} INR`);
 
 }, 2000);
+
+async function main() {
+    const highestBid = solInrMarket.getRelevantDepth().highestBid;
+    await createOrder("buy", "XAIINR", parseFloat((highestBid + 0.001).toFixed(3)), 1, Math.random().toString());
+    await new Promise((r) => setTimeout(() => r, 10000));
+    await cancelAll("XAIINR");
+    await new Promise((r) => setTimeout(r, 1000));
+    main();
+}
+
+setInterval(async () => {
+    main();
+}, 20000);
